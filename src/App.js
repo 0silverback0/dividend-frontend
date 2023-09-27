1846088b05d45de07280644199a5e399baa20929
SilverBack Gorilla <silverbackgorilla@SilverBacks-MacBook-Pro.local>
2023-09-26 22:53:49 -0400

    remove comments App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,3 @@
-// // App.js
-
-// import React, { useState } from 'react';
-// import TickerForm from './TickerForm';
-// import DataTable from './DataTable';
-// import Header from './Header';
-// import { fetchDividendData } from './dividendService'; // Import your API service
-
-// function App() {
-//   const [data, setData] = useState([]);
-
-//   const handleSymbolSubmit = (symbol) => {
-//     fetchDividendData(symbol)
-//       .then((response) => {
-//         setData(response); // Assuming your API response contains a 'data' property
-//       })
-//       .catch((error) => {
-//         console.error('Error fetching data:', error);
-//       });
-//   };
-
-//   return (
-//     <div>
-//       <Header />
-//       <TickerForm onSymbolSubmit={handleSymbolSubmit} />
-//       <DataTable data={data} />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useState } from 'react';
 import TickerForm from './TickerForm';
 import DataTable from './DataTable';
